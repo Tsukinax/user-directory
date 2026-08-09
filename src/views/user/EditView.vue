7869f324d841db2af0fc9ee4f839e06043d6d77d
Tsukinax <hushdalanphop@gmail.com>
2026-08-10 06:51:12 +0700

    lab04 §6.1: add user Edit view with Update button + nested route + Edit nav link (staff-check 6.1); install pinia + nprogress deps

---
+++ src/views/user/EditView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+import type { User } from '@/types'
+
+const props = defineProps<{
+  user: User
+}>()
+const { user } = toRefs(props)
+const router = useRouter()
+const update = () => {
+  router.push({ name: 'user-list-view' })
+}
+</script>
+
+<template>
+  <p>Edit the details of {{ user.name }} here</p>
+  <button @click="update">Update</button>
+</template>
--- src/views/user/LayoutView.vue
+++ src/views/user/LayoutView.vue
@@ -36,6 +36,8 @@ onMounted(() => {
       <RouterLink :to="{ name: 'user-profile-view' }">Profile</RouterLink>
       |
       <RouterLink :to="{ name: 'user-posts-view' }">Posts</RouterLink>
+      |
+      <RouterLink :to="{ name: 'user-edit-view' }">Edit</RouterLink>
     </nav>
     <RouterView :user="user" />
   </div>

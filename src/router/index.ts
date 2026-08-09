1b9beef2637a22759c328d73f55658c08e6dc96b
Tsukinax <hushdalanphop@gmail.com>
2026-08-10 06:52:42 +0700

    lab04 §6.3: global NProgress loading bar on every route change (main.ts css + router.beforeEach/afterEach) (staff-check 6.3)

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import UserProfileView from '@/views/user/ProfileView.vue'
 import UserPostsView from '@/views/user/PostsView.vue'
 import UserEditView from '@/views/user/EditView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import nProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -49,6 +50,14 @@ const router = createRouter({
       component: NotFoundView,
     },
   ],
+})
+
+router.beforeEach(() => {
+  nProgress.start()
+})
+
+router.afterEach(() => {
+  nProgress.done()
 })
 
 export default router
